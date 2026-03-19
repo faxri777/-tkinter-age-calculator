57aab9776da077ea1058b4c2eee123e1d75c27b1
faxriddin-creator <faxriddintoxtasinov988@gmail.com>
2026-03-19 10:12:47 -0700

    Create age-calculator.py

---
+++ age-calculator.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from tkinter import *
+
+oyna = Tk()
+oyna.title('dasturcha')
+oyna.geometry('300x300')
+
+natija = Label(text="yoshingiz:", bg="white")
+natija.place(x=90, y=135, width=120, height=40)
+
+yil = Entry()
+yil.place(x=75, y=50, width=150, height=30)
+
+def farq():
+	bugun = datetime.today()
+	natija.config(text=bugun.year - int(yil.get())) 
+	 
+	     
+tugma = Button(text="hisoblash", command=farq)
+tugma.place(x=90, y=90, width=120, height=40)
+
+
+oyna.mainloop()
